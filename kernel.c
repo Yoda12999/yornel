674e68ee0e40a0a2e5b97a4f60c54cb66f484b62
Gabriel Smith <ga29smith@gmail.com>
2016-01-02 21:41:54 -0500

    Added support for the shift key and future support for other modifiers

--- kernel.c
+++ kernel.c
@@ -53,6 +53,22 @@ extern char read_port(unsigned short port);
 extern void write_port(unsigned short port, unsigned char data);
 
 unsigned int cursor_loc = 0;
+unsigned char tab_size = 4;
+
+struct Mod_keys {
+	unsigned char shift :2;		//set with either shift key
+	unsigned char r_shift :1;	//set with only the right shift key
+	unsigned char ctrl :2;
+	unsigned char r_ctrl :1;
+	unsigned char alt :2;
+	unsigned char r_alt :1;
+	unsigned char meta :2;
+	unsigned char r_meta :1;
+	unsigned char clck :1;
+	unsigned char nlck :1;
+	unsigned char plck :1;
+} mod_keys;
+
 char *vidptr = (char*) 0xb8000;	// video mem starts here
 
 struct GDT_entry {
@@ -74,20 +90,20 @@ struct GDT_entry {
 	unsigned char base_high;
 };
 
-#define GDT_ENTRY(gdt_type, gdt_base, gdt_limit, gdt_dpl) {\
-	.limit_low	= (((gdt_limit) >> 12) & 0xFFFF),    \
-	.base_low	= ((gdt_base) & 0xFFFF),             \
-	.base_mid	= (((gdt_base) >> 16) & 0xFF),       \
-	.type = gdt_type,                                 \
-	.one = 1,                                         \
-	.dpl = gdt_dpl,                                   \
-	.present = 1,                                     \
-	.limit_high = ((gdt_limit) >> 28),                     \
-	.available = 0,                                    \
-	.zero = 0,                                        \
-	.op_size = 1,                                     \
-	.gran = 1,                                        \
-	.base_high = (((gdt_base) >> 24) & 0xFF),         \
+#define GDT_ENTRY(gdt_type, gdt_base, gdt_limit, gdt_dpl) {	\
+	.limit_low	= (((gdt_limit) >> 12) & 0xFFFF),			\
+	.base_low	= ((gdt_base) & 0xFFFF),					\
+	.base_mid	= (((gdt_base) >> 16) & 0xFF),				\
+	.type = gdt_type,										\
+	.one = 1,												\
+	.dpl = gdt_dpl,											\
+	.present = 1,											\
+	.limit_high = ((gdt_limit) >> 28),						\
+	.available = 0,											\
+	.zero = 0,												\
+	.op_size = 1,											\
+	.gran = 1,												\
+	.base_high = (((gdt_base) >> 24) & 0xFF),				\
 }
 
 struct GDT_entry GDT[GDT_SIZE] = {
@@ -167,28 +183,21 @@ void idt_init(void) {
 	load_idt(&idt_ptr);
 }
 
-void kb_init(void) {
-	// 0xFD = 11111101 - IRQ1 only enabled, keyboard
-	write_port(0x21, 0xFD);
-}
-
-void keyboard_handler_main(void) {
-	unsigned char status;
-	char keycode;
-	
-	// write EOI to allow more interrupts
-	write_port(PIC1_CMD, 0x20);
-	
-	status = read_port(KEYBOARD_STATUS);
-	// lowest bit set if buffer empty
-	if(status & 0x01) {
-		keycode = read_port(KEYBOARD_DATA);
-		if(keycode <= 0) {
+void kprint_char(char c) {
+	switch(c) {
+		case '\n':
+			cursor_loc += COLUMNS;
 			return;
-		}
-		vidptr[cursor_loc*2] = keyboard_map[keycode];
-		vidptr[cursor_loc*2 + 1] = 0x07;
-		cursor_loc++;
+		case '\r':
+			cursor_loc -= cursor_loc%COLUMNS;
+			return;
+		case '\t':
+			cursor_loc += tab_size;
+			return;
+		default:
+			vidptr[cursor_loc*2] = c;
+			vidptr[cursor_loc*2 + 1] = 0x07;
+			cursor_loc++;
 	}
 }
 
@@ -196,15 +205,104 @@ void kprint(const char *str) {
 	unsigned int i = 0;
 
 	while(str[i] != '\0') {
-		vidptr[cursor_loc*2] = str[i];
-		vidptr[cursor_loc*2 + 1] = 0x07;
+		kprint_char(str[i]);
 		i++;
-		cursor_loc++;
 	}
 }
 
-void kprintln(void) {
-	cursor_loc += COLUMNS;
+void kprint_num(int num, unsigned int radix) {
+	if(num < 0) {
+		num = -num;
+		kprint_char('-');
+	}
+	
+	if(num/radix) {
+		kprint_num(num/radix, radix);
+	}
+	
+	if(num%radix <= 9) {
+		kprint_char(num%radix + '0');
+	} else {
+		kprint_char(num%radix + 'a' - 10);
+	}
+}
+
+void kb_init(void) {
+	unsigned char code;
+	
+	// 0xFD = 11111101 - IRQ1 only enabled, keyboard
+	write_port(0x21, 0xFD);
+}
+
+void keyboard_handler_main(void) {
+	unsigned char status;
+	unsigned char scancode;
+	unsigned char keycode;
+	
+	// write EOI to allow more interrupts
+	write_port(PIC1_CMD, 0x20);
+	
+	status = read_port(KEYBOARD_STATUS);
+	// lowest bit set if buffer empty
+	while(status & 0x01) {
+		scancode = read_port(KEYBOARD_DATA);
+		
+		if(scancode >= 0x80) {
+			// released key
+			switch(scancode - 0x80) {
+				case RSFT_CODE:
+					mod_keys.r_shift = 0;
+				case LSFT_CODE:
+					mod_keys.shift -= 1;
+					break;
+			}
+			return;
+		}
+		
+		switch(scancode) {
+			case 0xe0:
+			case 0xe1:
+				// escaped keycodes
+				status = read_port(KEYBOARD_STATUS);
+				if(status & 0x01) {
+					scancode = read_port(KEYBOARD_DATA);
+					if(scancode >= 0x80) {
+						// released key
+					}
+				}
+				break;
+			case RSFT_CODE:
+				mod_keys.r_shift = 1;
+			case LSFT_CODE:
+				mod_keys.shift += 1;
+				break;
+			default:
+				keycode = keyboard_map[scancode];
+				
+				if(mod_keys.shift && scancode >= 2 && scancode < 54) {
+					keycode = shift_map[scancode - 2];
+					if(mod_keys.clck && keycode >= 'A' && keycode <= 'Z') {
+						keycode += 'a' + 'A';
+					}
+				} else if(mod_keys.clck && keycode >= 'A' && keycode <= 'Z') {
+					keycode += 'a' + 'A';
+				}
+				
+				if(keycode == 0) {
+					return;
+				} else if(keycode == '\n') {
+					kprint_char('\r');
+				}
+				kprint_char(keycode);
+		}
+		
+		/*
+		kprint_num(scancode, 16);
+		kprint_char(' ');
+		*/
+		
+		status = read_port(KEYBOARD_STATUS);
+	}
 }
 
 void clear_screen(void) {
@@ -219,14 +317,12 @@ void clear_screen(void) {
 }
  
 void kmain(void) {
-	const char *str = "Welcome to Yornel";
+	const char *str = "Welcome to Yornel\n\n\r";
 	
 	clear_screen();
 	
 	// splash screen
 	kprint(str);
-	kprintln();
-	kprintln();
 	
 	// init
 	gdt_init();
--- keyboard_map.h
+++ keyboard_map.h
@@ -4,41 +4,79 @@
  * All credits where due
  */
 
+#define ESC 0
+
+#define LSFT_CODE 0x2a
+#define RSFT_CODE 0x36
+#define LSFT 0
+#define RSFT 0
+
+#define LCTL 0
+#define LALT 0
+
+#define META 0
+
+#define CLCK 0
+#define NLCK 0
+#define SLCK 0
+
+#define INS 0
+#define HOME 0
+#define PGUP 0
+#define DEL 127
+#define END 0
+#define PGDN 0
+
+#define UPAR 0
+#define LTAR 0
+#define RTAR 0
+#define DNAR 0
+
+#define F1 0
+#define F2 0
+#define F3 0
+#define F4 0
+#define F5 0
+#define F6 0
+#define F7 0
+#define F8 0
+#define F9 0
+#define F10 0
+#define F11 0
+#define F12 0
+
 unsigned char keyboard_map[128] = {
-	0, 27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',
-	'\b' /* Backspace */, '\t' /* Tab */,			
-	'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']',
-	'n' /* Enter key */, 0 /* 29 - Control */,
-	'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',
-	0 /* Left shift */,	
-	'\\', 'z', 'x', 'c', 'v', 'b', 'n',	'm', ',', '.', '/',
-	0 /* Right shift */,
-	'*',
-    0 /* Alt */,
-	' ' /* Space bar */,
-    0 /* Caps lock */,
-    0 /* 59 - F1 */, 0, 0, 0, 0, 0, 0, 0, 0, 0 /* F10 */,
-    0 /* 69 - Num lock */,
-    0 /* Scroll Lock */,
-    0 /* Home key */,
-    0 /* Up Arrow */,
-    0 /* Page Up */,
-	'-',
-    0 /* Left Arrow */,
-    0,
-    0 /* Right Arrow */,
-	'+',
-    0 /* 79 - End key*/,
-    0 /* Down Arrow */,
-    0 /* Page Down */,
-    0 /* Insert Key */,
-    0 /* Delete Key */,
-    0, 0, 0,
-    0 /* F11 Key */, 0 /* F12 Key */,
-    0 /* 89 */,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0 /* 99 */,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0 /* 109 */,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0 /* 119 */,
-    0, 0, 0, 0, 0, 0, 0, 0 /* 127 */
+	0, ESC, '1', '2', '3', '4', '5', '6', '7', '8',
+	'9', '0', '-', '=', '\b', '\t',	'q', 'w', 'e', 'r',
+	't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', LCTL,
+	'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
+	'\'', '`', LSFT, '\\', 'z', 'x', 'c', 'v', 'b', 'n',
+	'm', ',', '.', '/', RSFT, '*', LALT, ' ', CLCK, F1,
+	F2, F3, F4, F5, F6, F7, F8, F9, F10, NLCK,
+	/* Keypad */
+	SLCK, HOME, UPAR, PGUP, '-', LTAR, '5', RTAR, '+', END,
+	DNAR, PGDN, INS, DEL, 0, 0, 0, F11, F12, 0,
+	
+	//don't exist on normal keyboards
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0 /* 99 */,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0 /* 109 */,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0 /* 119 */,
+	0, 0, 0, 0, 0, 0, 0, 0 /* 127 */
+};
+
+unsigned char shift_map[52] = {
+	/* 2 offset */
+	'!', '@', '#', '$', '%', '^', '&', '*',
+	'(', ')', '_', '+', '\b', '\t', 'Q', 'W', 'E', 'R',
+	'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n', LCTL,
+	'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':',
+	'"', '~', LSFT, '|', 'Z', 'X', 'C', 'V', 'B', 'N',
+	'M', '<', '>', '?'
+};
+
+unsigned char nlck_map[13] = {
+	/* Keypad - 71 offset*/
+	'7', '8', '9', '-', '4', '5', '6', '+', '1',
+	'2', '3', '0', '.'
 };
 
